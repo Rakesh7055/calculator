4193861a9d8ae3ff8f1faf74d400d1ace06d6e18
Rakesh7055 <rakeshsinghss970@gmail.com>
2023-05-04 22:45:01 +0530

    fix all

--- calu.js
+++ calu.js
@@ -1,23 +1,26 @@
 let string = "";
+console.log(string);
 let buttons = document.querySelectorAll("button");
-Array.from(buttons).forEach((button)=>{
-   button.addEventListener('click',(e)=>{
-       if(e.target.innerHTML == "="){
-           string = eval(string);
-           document.querySelector('input').value = string;
-       }
-       else if(e.target.innerHTML == "C"){
-           string = "";
-           document.querySelector("input").value = string;
-       }
-       else if(e.target.innerHTML == "x"){
-           string = pop;
-           document.querySelector("input").value = string;
-       }
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "C") {
+      string = "";
+      document.querySelector("input").value = string;
+    } else if (e.target.innerHTML == "x") {
+      string = string.slice(0, string.length - 1);
+      document.querySelector("input").value = string;
+    }
+    // } else if(e.target.innerHTML == "%"){
+    //     string = (function(x,y){
 
-       else{console.log(e.target);
-       string = string + e.target.innerHTML;
-       document.querySelector('input').value = string;
-       }
-   })
-})
+    //     })
+    // }
+    else {
+      string = string + e.target.innerHTML;
+      document.querySelector("input").value = string;
+    }
+  });
+});
